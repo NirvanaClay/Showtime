fec726edba892b55fca9bc23ef4fe50c5b318e49
Clay <thejourney318@gmail.com>
2022-06-26 19:45:46 -0400

    Updating error log driver.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,11 +65,10 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return "Fuck you";
-        // return User::create([
-        //     // 'username' => $data['username'],
-        //     'email' => $data['email'],
-        //     'password' => Hash::make($data['password']),
-        // ]);
+        return User::create([
+            // 'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
     }
 }
