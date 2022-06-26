56af6f1f503d4e32b9d8a130d676d98563cbb2e7
Clay <thejourney318@gmail.com>
2022-06-26 18:46:30 -0400

    Changed assets to secure.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta id="token" name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="stylesheet" href="{{ asset('styles/reset.css') }}"> 
-        <link rel="stylesheet" href="{{ asset('styles/app.css') }}"> 
-        <link rel="stylesheet" href="{{ asset('styles/home.css') }}"> 
-        <link rel="stylesheet" href="{{ asset('styles/login.css') }}"> 
-        <link rel="stylesheet" href="{{ asset('styles/form.css') }}"> 
-        <link rel="stylesheet" href="{{ asset('styles/header.css') }}"> 
-        <link rel="stylesheet" href="{{ asset('styles/shows.css') }}"> 
-        <link rel="stylesheet" href="{{ asset('styles/register.css') }}"> 
+        <link rel="stylesheet" href="{{ secure_asset('styles/reset.css') }}"> 
+        <link rel="stylesheet" href="{{ secure_asset('styles/app.css') }}"> 
+        <link rel="stylesheet" href="{{ secure_asset('styles/home.css') }}"> 
+        <link rel="stylesheet" href="{{ secure_asset('styles/login.css') }}"> 
+        <link rel="stylesheet" href="{{ secure_asset('styles/form.css') }}"> 
+        <link rel="stylesheet" href="{{ secure_asset('styles/header.css') }}"> 
+        <link rel="stylesheet" href="{{ secure_asset('styles/shows.css') }}"> 
+        <link rel="stylesheet" href="{{ secure_asset('styles/register.css') }}"> 
 
         <title>Laravel</title>
 
