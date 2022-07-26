820620e8115a5b8648752082d85fc621abecd32b
Clay <thejourney318@gmail.com>
2022-07-26 16:36:17 -0400

    ..

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const path = require('path');
-const express = require('express');
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
