adc70bd925f212daa8b90e423614e7d4af947810
Clay <thejourney318@gmail.com>
2022-07-26 23:03:14 -0400

    ..

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const express = require("express");
-// eslint-disable-next-line no-unused-vars
-// const bodyParser = require('body-parser');
-const path = require("path");
-const app = express();
-const port = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, "build")));
-
-// This route serves the React app
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('build'));
+   app.get('*', (req, res) => {
+     res.sendFile(path.join('build', 'index.html'));
+   });
+ }
