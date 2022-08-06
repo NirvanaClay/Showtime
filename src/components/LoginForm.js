675d6da372d0140974b77ad53e27bcccd0579a1c
Clay <thejourney318@gmail.com>
2022-08-05 20:17:06 -0400

    ..

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -13,6 +13,7 @@ const LoginForm = ({ setLoginStatus, setUser, loginStatus, passwordVisibility, s
       email: e.target[0].value,
       password: e.target[1].value
     }
+    axios.get('/fuck')
     // await axios.get('/sanctum/csrf-cookie')
     // .then(res => {
     //   console.log("In initial sanctum get, res is:")
