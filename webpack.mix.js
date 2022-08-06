326f0a226af066399fd08471ba7d427ca9a17e6f
Clay <thejourney318@gmail.com>
2022-08-05 20:43:24 -0400

    ..

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('public/js/app.js', 'build/js')
-    .react('public/js/app.js', 'build/js')
+    .react()
     .sass('public/sass/app.scss', 'public/css');
