e0b9f5a916851d0b8194771d61285d23071f7e1a
Clay <thejourney318@gmail.com>
2022-08-15 17:36:51 -0400

    ..

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,10 +12,10 @@ try {
 
 window.axios = require('axios');
 
-// let token = document.getElementById('token')
+let token = document.getElementById('token')
 
 // window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-// window.axios.defaults.headers.common['Accept'] = 'application/json';
+window.axios.defaults.headers.common['XSRF-TOKEN'] = token;
 
 windows.axios.defaults.withCredentials = true;
 
