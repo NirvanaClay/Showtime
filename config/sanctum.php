7dde1d1fc6022618a7bf2039a19e0e027ebd7ea9
Clay <thejourney318@gmail.com>
2022-08-15 17:19:03 -0400

    ..

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3306,localhost:3000,127.0.0.1:3000,::1,https://www.showcheck.herokuapp.com,http://localhost, http://localhost:3000',
+        'localhost,localhost:3306,localhost:3000,127.0.0.1:3000,::1,https://www.showcheck.herokuapp.com,https://showcheck.herokuapp.com,http://localhost, http://localhost:3000',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
