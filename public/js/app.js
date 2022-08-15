e4fdf594d4dc924877e80795003033c170865fd1
Clay <thejourney318@gmail.com>
2022-08-14 21:09:50 -0400

    ..

--- public/js/app.js
+++ public/js/app.js
@@ -3427,10 +3427,11 @@ var LoginForm = function LoginForm(_ref) {
               _context.next = 4;
               return axios.get('/sanctum/csrf-cookie').then(function (res) {
                 console.log("In initial sanctum get, res is:");
-                console.log(res); // let token = res.config.headers.X-XSRF-TOKEN
+                console.log(res);
+                let token = res.config.headers.X-XSRF-TOKEN
 
-                axios.post('/login', data).then(function () {
-                  axios.get('/user').then(function (res) {
+                axios.post('/api/login', data).then(function () {
+                  axios.get('/api/user').then(function (res) {
                     var userInfo = res.data;
                     console.log("In login form, userInfo is:");
                     console.log(userInfo);
