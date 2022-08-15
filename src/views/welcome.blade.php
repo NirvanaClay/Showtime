d4895af6e243e3b8d91e02fe480babcebebd24d0
Clay <thejourney318@gmail.com>
2022-08-15 17:24:59 -0400

    ..

--- src/views/welcome.blade.php
+++ src/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta id="token" name="csrf-token" content="{{ csrf_token() }}">
+        {{-- <meta id="token" name="csrf-token" content="{{ csrf_token() }}"> --}}
 
         <link rel="stylesheet" href="{{ secure_asset('styles/reset.css') }}"> 
         <link rel="stylesheet" href="{{ secure_asset('styles/app.css') }}"> 
